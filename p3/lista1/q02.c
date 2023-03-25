0155e70f348c3af69f450794aee57ba047fd1782
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2023-03-24 21:38:35 -0300

    q02

--- p3/lista1/q02.c
+++ p3/lista1/q02.c
@@ -5,11 +5,23 @@
  Observações:
 */
 
-include<stdio.h>
+#include<stdio.h>
 
 int main(){
+	float salarioLiquido, horaAula, horasAulasDadas, descontoINSS;
 	
+	printf("Informe o valor da hora aula:");
+	scanf("%f", &horaAula);
 	
+	printf("Informe a quantidade de horas aulas dadas:");
+	scanf("%f", &horasAulasDadas);
+	
+	printf("Informe o desconto do INSS:");
+	scanf("%f", &descontoINSS);
+	
+	salarioLiquido = (horaAula * horasAulasDadas) - ((horaAula * horasAulasDadas)*(descontoINSS/100));
+	
+	printf("O salario liquido do professor eh: %f", salarioLiquido);
 	
 	return 1;
 }

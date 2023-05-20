8c151363e5d6f54030666c9bc009d8a07813c478
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2023-05-20 19:58:50 -0300

    q05full

--- p3/lista3/ED-lista2-questao05.c
+++ p3/lista3/ED-lista2-questao05.c
@@ -1,5 +1,5 @@
 /*
-** Função : Imprime todos os pares de primos, que somados resultam em pares entre 700 e 1100
+** Função : Imprime todos os pares de primos maiores que somados são maiores que 2 e vão até a entrada do usuário
 ** Autor : Daniel Warella Pitsch
 ** Data : 20/05/2023
 ** Observações:
@@ -12,16 +12,18 @@ int aleatorio(int n);
 bool ehPrimo(int numero);
 
 int main(){
+	int numero, i, soma=0, n1, n2, pares=4;
 	
-	int i, soma=0, n1, n2, pares=700;
+	printf("Informe o maior numero: ");
+	scanf("%d", &numero);
 	
-	for(i = pares; i<=1100; i+=2){
+	for(i = pares; i<=numero; i+=2){
 		while (soma != i){
 			n1 = aleatorio(650);
 			n2 = aleatorio(650);
 			if(ehPrimo(n1) == 1 && ehPrimo(n2) == 1){
 				soma = n1 + n2;
-				if(n1+n2 == i){
+				if(n1+n2 == i && soma > 2){
 					printf("%d + %d e a soma: %d \n", n1, n2, soma);
 				}
 			}
---
+++ p3/lista3/ED-lista2-questao06.c
@@ -0,0 +1,15 @@
+/*
+** Função : Imprime todos os pares de primos maiores que somados são maiores que 2 e vão até a entrada do usuário
+** Autor : Daniel Warella Pitsch
+** Data : 20/05/2023
+** Observações:
+*/
+
+#include<stdio.h>
+
+int main(){
+	
+	
+	printf("-Fim-");
+	return 0;
+}

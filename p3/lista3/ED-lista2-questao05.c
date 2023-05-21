990ac25bcb75e03145838c240054e3228023b9a7
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2023-05-20 22:33:00 -0300

    q06full

--- p3/lista3/ED-lista2-questao05.c
+++ p3/lista3/ED-lista2-questao05.c
@@ -12,7 +12,7 @@ int aleatorio(int n);
 bool ehPrimo(int numero);
 
 int main(){
-	int numero, i, soma=0, n1, n2, pares=4;
+	int numero, i, soma=0, n1, n2, pares=2;
 	
 	printf("Informe o maior numero: ");
 	scanf("%d", &numero);
@@ -20,10 +20,10 @@ int main(){
 	for(i = pares; i<=numero; i+=2){
 		while (soma != i){
 			n1 = aleatorio(650);
-			n2 = aleatorio(650);
-			if(ehPrimo(n1) == 1 && ehPrimo(n2) == 1){
+			n2 = aleatorio(numero);
+			if(ehPrimo(n1) == 1 && ehPrimo(n2) == 1 && n1 !=0 && n2!=0){
 				soma = n1 + n2;
-				if(n1+n2 == i && soma > 2){
+				if(n1+n2 == i ){
 					printf("%d + %d e a soma: %d \n", n1, n2, soma);
 				}
 			}
--- p3/lista3/ED-lista2-questao06.c
+++ p3/lista3/ED-lista2-questao06.c
@@ -1,15 +1,38 @@
 /*
-** Função : Imprime todos os pares de primos maiores que somados são maiores que 2 e vão até a entrada do usuário
+** Função : o programa receber um numero e retorna sua raiz quadrada usando o método de Newton-Raphson
 ** Autor : Daniel Warella Pitsch
 ** Data : 20/05/2023
-** Observações:
+** Observações: f(x) = x - cos(x); x0= 1; f'(x) = 1 + sin(x); xn = x - f(x) / f'(x).
 */
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 
-int main(){
+float raiz_quadrada(float n);
+
+int main() {
+  float n, raiz; 
+  printf("Digite o valor de n: "); 
+  scanf("%f", &n); 
+
+  raiz = raiz_quadrada(n); 
+
+  printf("\nA raiz quadrada %.1f eh %.5f", n, raiz); ;
 	
-	
-	printf("-Fim-");
+	printf("\n-Fim-");
 	return 0;
+}
+
+float raiz_quadrada(float n) {
+  float x0 = 1.0, x1, e = 0.0001, dif; 
+
+  do {
+    x1 = x0 - (x0 * x0 - n) / (2 * x0); 
+    dif = fabs(x1 - x0); 
+    x0 = x1; 
+    //printf("dif: %f e x1: %f\n", dif, x1);
+  } while (dif > e); 
+
+  return x1;
 }

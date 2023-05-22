51dfcd3b1cefbf5aec21f06a233a560cbefcc6ba
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2023-05-22 17:30:04 -0300

    fim

--- p3/lista3/ED-lista2-questao08.c
+++ p3/lista3/ED-lista2-questao08.c
@@ -6,78 +6,7 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct complexo {
-  double real; 
-  double imag; 
-} Complexo;
-
-
-Complexo* cria_complexo(double real, double imag) {
-  Complexo* c = (Complexo*) malloc(sizeof(Complexo)); 
-  if (c == NULL) { 
-    printf("Erro de alocacao de memoria\n"); 
-    exit(1); 
-  }
-  c->real = real; 
-  c->imag = imag; 
-  return c; 
-}
-
-
-void destroi_complexo(Complexo* c) {
-  free(c); 
-}
-
-
-Complexo* le_complexo() {
-  double real, imag; 
-  printf("Digite a parte real do numero complexo: "); 
-  scanf("%lf", &real); 
-  printf("Digite a parte imaginária do numero complexo: "); 
-  scanf("%lf", &imag); 
-
-  return cria_complexo(real, imag); 
-}
-
-
-Complexo* soma_complexos(Complexo* c1, Complexo* c2) {
-  double real = c1->real + c2->real; 
-  double imag = c1->imag + c2->imag; 
-
-  return cria_complexo(real, imag); 
-}
-
-Complexo* subtrai_complexos(Complexo* c1, Complexo* c2) {
-  double real = c1->real - c2->real; 
-  double imag = c1->imag - c2->imag; 
-
-  return cria_complexo(real, imag); 
-}
-
-Complexo* multiplica_complexos(Complexo* c1, Complexo* c2) {
-  double real = c1->real * c2->real - c1->imag * c2->imag; 
-  double imag = c1->real * c2->imag + c1->imag * c2->real; 
-
-  return cria_complexo(real, imag); 
-}
-
-Complexo* divide_complexos(Complexo* c1, Complexo* c2) {
-  if (c2->real == 0 && c2->imag == 0) { 
-    printf("\nErro: Impossivel divisao por zero!"); 
-    exit(1); 
-  }
-
-  double denom = c2->real * c2->real + c2->imag * c2->imag; 
-  double real = (c1->real * c2->real + c1->imag * c2->imag) / denom; 
-  double imag = (c1->imag * c2->real - c1->real * c2->imag) / denom; 
-
-  return cria_complexo(real, imag); 
-}
-
-void mostra_complexo(Complexo* c) {
-  printf("(%lf,%lf)", c->real, c->imag); 
-}
+#include "Complexo.h"
 
 int main() {
 	printf("Informe dois numeros complexos");
